c482e7b8b62440da901b07504212e085b051f13f
ahadkhaan97 <ahad.sartaj.ew@gmail.com>
2023-03-27 20:21:58 +0500

    submitted

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,113 +1,54 @@
 fun main() {
-    println(solution2("ab123", "ab34z"))
+    println(solution("ab123", "ab34z"))
 }
-
-fun solution2(s1: String, s2: String): Boolean {
-    val tokens1 = mutableListOf<String>()
-    val tokens2 = mutableListOf<String>()
-    var token = ""
-    for (i in s1.indices) {
-        if (s1[i] in '0'..'9') {
-            token += s1[i]
-            continue
-        }
-        if (token.isNotEmpty()) {
-            tokens1.add(token)
-            token = ""
-        }
-        tokens1.add(s1[i].toString())
-    }
-    if (token.isNotEmpty()) {
-        tokens1.add(token)
-        token = ""
-    }
-
-    for (i in s2.indices) {
-        if (s2[i] in '0'..'9') {
-            token += s2[i]
-            continue
-        }
-        if (token.isNotEmpty()) {
-            tokens2.add(token)
-            token = ""
-        }
-        tokens2.add(s2[i].toString())
-    }
-    if (token.isNotEmpty()) {
-        tokens2.add(token)
-    }
-
-
-}
-
 
 fun solution(s1: String, s2: String): Boolean {
-    val tokens1 = mutableListOf<String>()
-    val tokens2 = mutableListOf<String>()
-    var token = ""
-    for (i in s1.indices) {
-        if (s1[i] in '0'..'9') {
-            token += s1[i]
-            continue
-        }
-        if (token.isNotEmpty()) {
-            tokens1.add(token)
-            token = ""
-        }
-        tokens1.add(s1[i].toString())
-    }
-    if (token.isNotEmpty()) {
-        tokens1.add(token)
-        token = ""
-    }
-
-    for (i in s2.indices) {
-        if (s2[i] in '0'..'9') {
-            token += s2[i]
-            continue
-        }
-        if (token.isNotEmpty()) {
-            tokens2.add(token)
-            token = ""
-        }
-        tokens2.add(s2[i].toString())
-    }
-    if (token.isNotEmpty()) {
-        tokens2.add(token)
-    }
-
-    if (tokens1.size < tokens2.size) {
-        for (i in 0 until tokens1.size) {
-            if (!isNumeric(tokens1[i]) && !isNumeric(tokens2[i])
-                && tokens1[i].single().code > tokens2[i].single().code
-            ) {
-                return false
+    val s1Tokens = tokenize(s1)
+    val s2Tokens = tokenize(s2)
+    var lastZero: Int? = null
+    for (i in s1Tokens.indices) {
+        if (i >= s2Tokens.size) return false
+        val t1 = s1Tokens[i]
+        val t2 = s2Tokens[i]
+        if (t1[0] in '0'..'9') {
+            if (t2[0] in '0'..'9') {
+                if (t1.toBigInteger() > t2.toBigInteger()) {
+                    return false
+                } else if (t2.toBigInteger() > t1.toBigInteger()) {
+                    return true
+                } else if (t1.length != t2.length && lastZero == null) {
+                    lastZero = t1.length - t2.length
+                }
+            } else {
+                return true
             }
-
-            if (isNumeric(tokens1[i]) && isNumeric(tokens2[i]) && tokens1[i].toInt() > tokens2[i].toInt()) {
+        } else {
+            if (t1 > t2) {
+                return false
+            } else if (t2 > t1) {
                 return true
             }
         }
-        return true
     }
-    if (tokens1.size > tokens2.size)
-        return false
-
-    for (i in 0 until tokens1.size) {
-        if (isNumeric(tokens1[i]) && isNumeric(tokens2[i]) && tokens1[i].toInt() < tokens2[i].toInt()) {
-            return true
-        } else if (isNumeric(tokens1[i]) && !isNumeric(tokens2[i])) {
-            return true
-        } else if (!isNumeric(tokens1[i]) && !isNumeric(tokens2[i])
-            && tokens1[i].single().code < tokens2[i].single().code
-        ) {
-            return true
-        }
-    }
-
+    if (s1Tokens.size < s2Tokens.size) return true
+    if (lastZero != null && lastZero > 0) return true
     return false
 }
 
-fun isNumeric(str: String): Boolean {
-    return str.toDoubleOrNull() != null
+fun tokenize(s: String): List<String> {
+    var i = 0
+    val tokens = mutableListOf<String>()
+    while (i < s.length) {
+        if (s[i] in '0'..'9') {
+            var token = ""
+            while (i < s.length && s[i] in '0'..'9') {
+                token += s[i++]
+            }
+            tokens.add(token)
+        } else {
+            tokens.add(s[i++].toString())
+        }
+    }
+    return tokens
 }
+
